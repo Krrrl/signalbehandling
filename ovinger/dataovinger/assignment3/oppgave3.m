72ed7f24788a48e51eafd1a14239871ed6c233b9
krrrl <km.ylvisaker@gmail.com>
2019-02-18 09:46:34 +0100

    Added error text to remember what to ask TA about.

--- ovinger/dataovinger/assignment3/oppgave3.m
+++ ovinger/dataovinger/assignment3/oppgave3.m
@@ -8,3 +8,7 @@ fanalyse(ecg, 512, 512);
 ecg3 = [ecg, ecg, ecg];
 
 fanalyse(ecg3, 1536, 512);
+
+%her er det noe galt, ettersom absoluttverdi plottene ikke gjennspeiler
+%antallet koeffisienter man trenger for å rekonstruere signalet. Ref øving
+%2.
